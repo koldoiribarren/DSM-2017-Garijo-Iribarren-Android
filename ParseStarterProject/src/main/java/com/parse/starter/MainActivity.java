9fb706b07e9f7854d9b78766ab31e13d3eb309e9
Koldo Iribarren <master@koldoiribarren.com>
2017-05-17 20:50:29 +0200

    Añadida pantalla de carga

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,10 +9,14 @@
 package com.parse.starter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -21,6 +25,8 @@ import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
+      Handler handler = new Handler();
+      Runnable run1 = new Runnable(){
+          public void run(){
+              Intent intent = new Intent(getApplicationContext(), home.class);
+              startActivity(intent);
+          }
+      };
+      Runnable run2 = new Runnable(){
+          public void run(){
+              Intent intent = new Intent(getApplicationContext(), login.class);
+              startActivity(intent);
+          }
+      };
+
+      Random rand = new Random();
+      int value = rand.nextInt(10);
+
+      String[] sentences = {
+              "Loading...",
+              "A.K.A. Fakestagram",
+              "This isn\'t actually loading anything",
+              "IVAN EHT NIOJ",
+              "I think this app deserves a 10/10",
+              "Have fun",
+              "I can see you... ;)",
+              "And rolls, and rolls...",
+              "-Error: All files erased-",
+              "You should be doing something more productive"
+      };
+
       ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
       if (ParseUser.getCurrentUser().getUsername()!=null) {
@@ -38,16 +74,24 @@ public class MainActivity extends AppCompatActivity {
           Log.i("Current User", "User: " + ParseUser.getCurrentUser());
           Log.i("Current User Name", "User name: " + ParseUser.getCurrentUser().getUsername());
 
-          Intent intent = new Intent(getApplicationContext(), home.class);
-          startActivity(intent);
+          ImageView loading = (ImageView) findViewById(R.id.loadingIcon);
+          loading.animate().rotation(-1080).setDuration(3000);
+
+          TextView welcomeMSG = (TextView) findViewById(R.id.loadingText);
+          welcomeMSG.setText("Welcome back");
+
+          handler.postDelayed(run1,2000);
 
       } else {
 
-          Log.i("Current User", "User: " + ParseUser.getCurrentUser());
-          Log.i("Current User Name", "User name: " + ParseUser.getCurrentUser().getUsername());
+          ImageView loading = (ImageView) findViewById(R.id.loadingIcon);
+          loading.animate().rotation(-1800).setDuration(6000);
 
-          Intent intent = new Intent(getApplicationContext(), login.class);
-          startActivity(intent);
+          TextView welcomeMSG = (TextView) findViewById(R.id.loadingText);
+          welcomeMSG.setText(sentences[value]);
+
+          handler.postDelayed(run1,5000);
+
       }
   }
 }
